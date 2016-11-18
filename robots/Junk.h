51a545e2ca89f105f1946222b710fca986a2a48e
Eric Nylander <eriny656@student.liu.se>
2016-11-18 14:59:24 +0100

    Robots added

---
+++ robots/Junk.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) David Wolfe, 1999.  All rights reserved.
+ * Ported to Qt and adapted for TDDD86, 2015.
+ */
+
+#ifndef JUNK_H
+#define JUNK_H
+
+#include "Robot.h"
+#include <QGraphicsScene>
+
+class Junk : public Robot {
+public:
+    Junk();
+    Junk(Robot c);
+
+    /*
+    * Draws this junk onto the given QGraphicsScene.
+    */
+    void draw(QGraphicsScene* scene) const;
+
+    void moveTowards(const Unit& u);
+
+    bool attacks(const Unit& u) const;
+};
+
+#endif // JUNK_H
